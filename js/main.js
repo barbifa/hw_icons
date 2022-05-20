9c5d4bcb05482ae41127001b44c1817104639ccb
barbifa <barbaraferrreiraaguiar@gmail.com>
2022-05-20 01:37:03 -0400

    adding js, html with the icons and hover effecy

--- js/main.js
+++ js/main.js
@@ -6,31 +6,17 @@
     // this is a JavaScript comment
     console.log('fired! javascript is working!');
 
-    // Step 1: go and find the elements on the page that you want the user to interact with
-
-    // querySelector takes a CSS selector and uses that to find a match in the HTML file
-
-    // let theBadge = document.querySelector('#png'),
-    //     theVector = document.querySelector('#vector');
-    // querySelector finds the first element that matches
-
-    // querySelectorAll finds ALL elements that match the selector
-    // and returns back an array-like object called a node list
-    let theBadges = document.querySelectorAll('img');
-
-    function logMyId() {
-        debugger; // pauses code execution at this line
-
-        // log the element's ID to the console window
+    const icons = document.querySelectorAll('svg');
+    console.log(icons); 
+    
+   
+    icons[0].addEventListener('click', function onClick() {
+      console.log(this.id);
+    });
+    
+    for (const icon of icons) {
+      icon.addEventListener('click', function onClick() {
         console.log(this.id);
-        // 'this' refers to the element that you're interacting with at the moment
+      });
     }
- 
-    // figure out how you want the user to interact with the badge graphic - a click, mouseover, mouseout, double click etc
-    // theBadge.addEventListener('click', logMyId);
-    // theVector.addEventListener('click', logMyId);
-
-    // for each badge in our collection, add an event handler
-    // arrow functions are just shorthand function declarations
-    theBadges.forEach(badge => badge.addEventListener('click', logMyId));
 })();
